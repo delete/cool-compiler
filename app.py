fc6f237ef0b12d9ae7177b0fcae3fcb4a503ed73
delete <pinheiro.llip@gmail.com>
2015-12-17 18:48:22 -0200

    interface web feita em Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template, request
+
+from analise_lex import tokenize
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+	llex = ()
+	lerror = ()
+	codigo = ''
+
+	if request.method == 'POST':
+		codigo = request.form['codigo']
+
+		llex, lerror = tokenize(codigo)
+
+	return render_template('index.html', llex=llex, lerror=lerror, codigo=codigo)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
