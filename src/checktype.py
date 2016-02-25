b92797cc8207cd71123a91dbe87646ca1da96eb3
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-02-25 09:21:30 -0300

    method that checks types

---
+++ src/checktype.py
@@ -0,0 +1,41 @@
+from ast import *
+from myexceptions import SemantError
+
+
+def returned_type(feature, _class):
+    try:
+        # For Attribute
+        _type = feature.type
+    except AttributeError:
+        # For Method
+        _type = feature.return_type
+
+    if _type == 'SELF_TYPE':
+        _type = _class.name
+
+    return _type
+
+
+def check_expression_type(expression, _class, scope):
+    """
+        Returns the type of the expression.
+
+        If the type is self, then returns the name of the
+        class.
+    """
+    if isinstance(expression, Str):
+        return 'String'
+
+    elif isinstance(expression, Object):
+        if expression.name == "self":
+            return _class.name
+
+        if not scope.exists(expression.name):
+            raise SemantError(
+                "Variable %s is not in scope" % expression.name
+            )
+
+        return scope.get(expression.name)
+
+    elif isinstance(expression, Int):
+        return "Int"
